fefa4cf2a318100767c13256c2ce6b08799054ab
Tim Kopp <tim.kopp@cyber-balance.com>
2021-01-24 12:38:18 -0500

    include settings

--- BTestAutomator/Program.cs
+++ BTestAutomator/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
-
+using Microsoft.Extensions.Configuration;
+using System.IO;
 namespace BTestAutomator
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            var val1 = builder.Build().GetSection("profiles").GetSection("csadmin").GetSection("user").Value;
+            var val2 = builder.Build().GetSection("profiles").GetSection("csadmin").GetSection("pass").Value;
+            Console.Write($"The values of parameters are: {val1} and {val2}");
+        
         }
     }
 }
